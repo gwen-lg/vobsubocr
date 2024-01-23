892e360a59e34f61f6814c5767cb93bcde5a00f0
Gwen Lg <me@gwenlg.fr>
2024-01-23 11:04:54 +0100

    enable profiling with puffin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use chrono::Local;
 use clap::Parser;
 use log::LevelFilter;
 use vobsubocr::{run, MemTracker, Opt, MEM_STATS};
@@ -6,12 +7,15 @@ use vobsubocr::{run, MemTracker, Opt, MEM_STATS};
 // Enable only on feature
 use tracking_allocator::{AllocationGroupToken, AllocationRegistry, Allocator};
 
-use std::alloc::System;
+use std::{alloc::System, fs::File};
 
 #[global_allocator]
 static GLOBAL: Allocator<System> = Allocator::system();
 
 fn main() -> anyhow::Result<()> {
+    let global_frame_view = puffin::GlobalFrameView::default();
+    puffin::set_scopes_on(true);
+
     let mem_tracker = MemTracker::new();
     AllocationRegistry::set_global_tracker(mem_tracker)
         .expect("no other global tracker should be set yet");
@@ -39,5 +43,17 @@ fn main() -> anyhow::Result<()> {
     drop(local_guard);
     AllocationRegistry::disable_tracking();
     MEM_STATS.print_mem_stats();
+
+    profiling::finish_frame!();
+    write_perf_file(global_frame_view);
+
     res
+}
+
+fn write_perf_file(global_frame_view: puffin::GlobalFrameView) {
+    let now = Local::now().format("%Y-%m-%d-%T").to_string();
+    let filename = format!("perf/capture_{}.puffin", now);
+    let mut file = File::create(filename).unwrap();
+    let frame_view = global_frame_view.lock();
+    frame_view.write(&mut file).unwrap();
 }
